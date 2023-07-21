f11e6ae22a12545d6fb14db09aec84bb34397e0c
HLlight <yongosy25@gmail.com>
2023-07-21 13:57:37 +0900

    커뮤니티 메인 틀

--- src/main/java/com/ja/safari/community/controller/CommunityController.java
+++ src/main/java/com/ja/safari/community/controller/CommunityController.java
@@ -1,18 +1,43 @@
 package com.ja.safari.community.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ja.safari.community.service.PromotionReviewServiceImpl;
+import com.ja.safari.dto.UserDto;
 
 @Controller
 @RequestMapping("/community/*")
 public class CommunityController {
 
+	@Autowired
+	private PromotionReviewServiceImpl promotionReviewService;
+	
 	  // 커뮤니티 메인 페이지
 	   @RequestMapping("mainPage")
-	   public String main() {
-	      
+	   public String main(Model model, HttpSession session) {
+		   
+//		   UserDto sessionUser = (UserDto)session.getAttribute("sessionUser");
+//
+//			int sessionId = 0;
+//
+//			if(sessionUser != null) {
+//				sessionId = sessionUser.getId();
+//			}
+		   
+	//	   List<Map<String, Object>> promoReviewList = promotionReviewService.getPromotionReviewList(sessionId);		
+		   
+		   
+	//	   model.addAttribute("promoReviewList", promoReviewList);
+		   
 	      return "community/mainPage";
 	   }
-
 
 }
